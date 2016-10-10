77b04ca6d733003936aec986ab58833392c4b344
Henrik Blidh <henrik.blidh@swedwise.com>
2016-10-10 15:33:21 +0200

    OCR storing in preparation of ML.

--- cowcv/cowparse/ocr/classify.py
+++ cowcv/cowparse/ocr/classify.py
@@ -13,30 +13,14 @@ from __future__ import print_function
 from __future__ import unicode_literals
 from __future__ import absolute_import
 
-import hashlib
-import io
-import pathlib
-import pickle
-
-from PIL import Image
+from cowcv.data.ocr import save_roi
 
 SAVE_IMAGES = True
-OCR_DATA_DIR = pathlib.Path(
-    __file__).resolve().parent.parent.parent.joinpath('data', 'ocr')
 
 
 def classify(img, bb):
     if SAVE_IMAGES:
-        with io.BytesIO() as bio:
-            Image.fromarray(img).save(bio, format='png')
-            bio.seek(0)
-            img_md5 = hashlib.md5(bio.read())
-            bio.seek(0)
-
-        with open(str(OCR_DATA_DIR.joinpath(str(img_md5) + '.png'))) as f:
-            f.write(bio.read())
-        with open(str(OCR_DATA_DIR.joinpath(str(img_md5) + '.pkl'))) as f:
-            pickle.dump(f, bb)
+        save_roi(img, bb)
 
     return None
 
--- cowcv/data/__init__.py
+++ cowcv/data/__init__.py
@@ -11,83 +11,36 @@ import imdirect
 imdirect.monkey_patch()
 from PIL import Image
 
-_COW1_URL = "https://dl.dropboxusercontent.com/u/21298554/cow1.jpg"
-_COW2_URL = "https://dl.dropboxusercontent.com/u/21298554/cow2.jpg"
-_COW3_URL = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/Kossa-2.jpg/800px-Kossa-2.jpg"
+_COW_URLS = {
+    1: "https://dl.dropboxusercontent.com/u/21298554/cow1.jpg",
+    2: "https://dl.dropboxusercontent.com/u/21298554/cow2.jpg",
+    3: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/Kossa-2.jpg/800px-Kossa-2.jpg",
+    4: "http://obj.imagedesk.se/obj/photo/3c/3cf3d13b57dd0a5d1a93cad63681c995.jpg",
+    5: "http://www.opps.se/roliga-kor-i-askloster-ko-bilder-168-ab.jpg",
+    6: "http://cdn.lovelylife.se/blogs/4/2015/12/Kor_LR.jpg",
+    7: "http://www.zolaenterprises.com/P1200084cs.jpg",
+    8: "http://www.opps.se/P1300244roliga-kor.jpg"
+}
+_COW_FACE_COORDS = {
+    1: (900, 500, 2650 - 900, 2150 - 800),
+    2: (450, 700, 3350 - 450, 3000 - 700),
+    3: None,
+    4: None
+}
 
-
-def cow1():
-    """The first test cow JPEG.
-
-    :return: The PIL image of first test cow.
-
-    """
+def get_cow(nbr=1):
     image_path = pathlib.Path(
-        __file__).resolve().parent.joinpath('cow1.jpg')
-    return _image(image_path, _COW1_URL)
+        __file__).resolve().parent.joinpath('cow{0}.jpg'.format(nbr))
+    if image_path.exists():
+        return Image.open(str(image_path))
+    else:
+        r = urlopen(_COW_URLS.get(nbr))
+        with open(str(image_path), 'wb') as f:
+            f.write(r.read())
+        return get_cow(nbr)
 
-
-def cow1_face_coordinates():
-    """
-    (960, 710), (2650, 2150)
-
-    :return:
-    """
-    x = 900
-    y = 800
-    w = 2650 - x
-    h = 2150 - y
-
-    return x, y, w, h
-
-
-def cow2():
-    """The second test cow JPEG.
-
-    :return: The PIL image of second test cow.
-
-    """
-    image_path = pathlib.Path(
-        __file__).resolve().parent.joinpath('cow2.jpg')
-    return _image(image_path, _COW2_URL)
-
-
-def cow2_face_coordinates():
-    """
-    (700, 450), (3350, 3000)
-
-    :return:
-    """
-    y = 700
-    x = 450
-    w = 3350 - x
-    h = 3000 - y
-
-    return x, y, w, h
-
-
-def cow3():
-    """The third test cow JPEG.
-
-    :return: The PIL image of third test cow.
-
-    """
-    image_path = pathlib.Path(
-        __file__).resolve().parent.joinpath('cow3.jpg')
-    return _image(image_path, _COW1_URL)
-
-
-def cow3_face_coordinates():
-    """
-
-    :return:
-    """
-    x = 0
-    y = 0
-    w = 2650 - x
-    h = 2150 - y
-
-    return x, y, w, h
+def get_cow_face_coordinates(nbr=1):
+    return _COW_FACE_COORDS.get(nbr)
 
 
 def _image(pth, url):
@@ -99,10 +52,4 @@ def _image(pth, url):
     :return: The :class:`~PIL.Image` requested.
 
     """
-    if pth.exists():
-        return Image.open(str(pth))
-    else:
-        r = urlopen(url)
-        with open(str(pth), 'wb') as f:
-            f.write(r.read())
-        return _image(pth, url)
+
